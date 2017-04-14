efb0d87b2da2b8461d4d3842ccd18ee1dd250380
hongru qi <hongru.qi@quvideo.com>
2017-04-14 11:36:30 +0800

    Initial commit

---
+++ Example/WTTableView/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  WTTableView
+//
+//  Created by lbrsilva-allin on 04/14/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WTTableView/WTViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTViewController.h
+//  WTTableView
+//
+//  Created by lbrsilva-allin on 04/14/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTViewController : UIViewController
+
+@end
