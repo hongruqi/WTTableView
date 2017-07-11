7e99dbecb13781208621e95a24038abffaab2144
hongruqi <qihongru1227@126.com>
2017-07-11 14:34:07 +0800

    update
    Update

--- WTTableView/Classes/WTTableView.h
+++ WTTableView/Classes/WTTableView.h
@@ -9,10 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "WTDataProvider.h"
 
+@class WTGenericModel;
+
+typedef void(^didSelectedBlock)(WTGenericModel *model);
+
 @interface WTTableView : UITableView
 
 @property (nonatomic, strong, readonly) NSMutableArray *dataSourceProvider;
 @property (nonatomic, weak) id<WTDataSourceProvider> dataProvider;
+@property (nonatomic, weak) id cellDelegate;
+@property (nonatomic, copy) didSelectedBlock selectedBlock;
 
 - (void)refreshData;
 
--- WTTableView/Classes/WTTableView.m
+++ WTTableView/Classes/WTTableView.m
@@ -47,15 +47,75 @@
     }
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSourceProvider.count;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
 {
     UITableViewCell <WTCellProtocol> *cell = nil;
     WTGenericModel *model = self.dataSourceProvider[indexPath.row];
     cell = [tableView dequeueReusableCellWithIdentifier:model.identifier forIndexPath:indexPath];
+    
     if ([cell conformsToProtocol:@protocol(WTCellProtocol)]) {
         [cell loadData:model];
     }
     
+    if ([cell respondsToSelector:@selector(setCellDelegate:)]) {
+        [cell setCellDelegate:self.cellDelegate];
+    }
+    
+    return cell;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row >= self.dataSourceProvider.count) {
+        return 44;
+    }
+    
+    WTGenericModel *model = self.dataSourceProvider[indexPath.row];
+    if (model.cellHeight > 0) {
+        return model.cellHeight;
+    }
+    
+    Class cellClass = NSClassFromString(model.identifier);
+    if ([cellClass respondsToSelector:@selector(cellHeight:)]) {
+        return [cellClass cellHeight:model];
+    }
+    
+    return 0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    if (self.dataSourceProvider.count > indexPath.row) {
+        WTGenericModel *model = self.dataSource[indexPath.row];
+        if (self.selectedBlock) {
+            self.selectedBlock(model);
+        }
+    }
+}
+
+- (void)insertData:(WTGenericModel *)data atIndex:(NSInteger)index
+{
+    [self.dataSourceProvider insertObject:data atIndex:index];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+    [self insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+- (void)deleteDataAtIndex:(NSInteger)index
+{
+    [self.dataSourceProvider removeObjectAtIndex:index];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+    [self deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+- (void)refreshData
+{
+    [self reloadData];
+}
+
 
 @end
